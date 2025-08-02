4b4d0f1f9155059da668eaf47bb4e9cdd048ad66
Lauty1550 <lautyjaime09@gmail.com>
2025-08-02 06:16:44 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./css/Root.css";
-
+import SwitchModeButton from "./components/SwitchModeButton";
 import Header from "./components/Header";
 import Content from "./components/Content";
+import { useDarkMode } from "./hooks/useDarkMode";
 
 function App() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <main className="root">
-      <Header></Header>
+      <Header>
+        switchModeButton=
+        {
+          <SwitchModeButton
+            darkMode={darkMode}
+            toggleDarkMode={toggleDarkMode}
+          />
+        }
+      </Header>
       <Content />
     </main>
   );

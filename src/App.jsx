f01db1792dc8a505a6a039ac2d342bcbcbab3216
Lauty1550 <lautyjaime09@gmail.com>
2025-08-02 06:13:21 -0300

    fix dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import "./css/Root.css";
-import SwitchModeButton from "./components/SwitchModeButton";
 
-import { useDarkMode } from "./hooks/useDarkmode";
 import Header from "./components/Header";
 import Content from "./components/Content";
 
 function App() {
-  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <main className="root">
-      <Header>
-        switchModeButton=
-        {
-          <SwitchModeButton
-            darkMode={darkMode}
-            toggleDarkMode={toggleDarkMode}
-          />
-        }
-      </Header>
+      <Header></Header>
       <Content />
     </main>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "../css/Header.css";
 // import LanguageSwitcher from "./LanguageSelector";
 
-export default function Header({ switchModeButton }) {
+export default function Header() {
   {
     return (
       <nav className="navbar">
         <main className="header">
           <h1 className="header-title">Kaiser Calc</h1>
 
-          {switchModeButton}
           {/* <LanguageSwitcher /> */}
         </main>
       </nav>

38278647f1aa2d0facb14f8ce1b8ad19d4fe1856
Lauty1550 <lautyjaime09@gmail.com>
2025-08-13 02:15:38 -0300

    i18n configurado, funcionando mas algunas traducciones, componente para el cambio de idioma

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,14 @@
-import React from "react";
 import "../css/Header.css";
-// import LanguageSwitcher from "./LanguageSelector";
+import { LanguageSwitcher } from "./LanguageSwitcher";
 
 export default function Header() {
-  {
-    return (
-      <nav className="navbar">
-        <main className="header">
-          <h1 className="header-title">Kaiser Calc</h1>
+  return (
+    <nav className="navbar">
+      <main className="header">
+        <h1 className="header-title">Kaiser Calc</h1>
 
-          {/* <LanguageSwitcher /> */}
-        </main>
-      </nav>
-    );
-  }
+        <LanguageSwitcher />
+      </main>
+    </nav>
+  );
 }
---
+++ src/components/LanguageSwitcher.jsx
@@ -0,0 +1,25 @@
+import { useTranslation } from "react-i18next";
+
+export function LanguageSwitcher() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem("i18nextLng", lng);
+    const path = window.location.pathname.split("/").filter(Boolean);
+    path[0] = lng;
+    window.history.replaceState({}, "", "/" + path.join("/"));
+  };
+
+  return (
+    <div>
+      <select
+        value={i18n.language}
+        onChange={(e) => changeLanguage(e.target.value)}
+      >
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
+    </div>
+  );
+}
